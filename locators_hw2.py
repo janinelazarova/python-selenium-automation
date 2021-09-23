62d366ec4cc5abc9db2edfca530941bfb949ace2
Janine Lazarova <janinelazarova@Janines-iMac.local>
2021-09-22 19:22:40 -0600

    HW2 - Ex.1

---
+++ locators_hw2.py
@@ -0,0 +1,35 @@
+"""
+//Amazon logo, search by XPATH
+driver.find_element(By.XPATH, "//i[@class='a-icon a-icon-logo']")
+
+//Email field, search by ID
+driver.find_element(By.ID, 'ap_email')
+
+//Continue button, search by ID
+driver.find_element(By.ID, 'continue')
+
+//Need help link, search by XPATH
+driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
+
+//Forgot your password link, search by ID
+driver.find_element(By.ID, 'auth-fpp-link-bottom')
+
+//Other issues with Sign-In link, search by ID
+driver.find_element(By.ID, 'ap-other-signin-issues-link')
+
+//Create your Amazon account button, search by ID
+driver.find_element(By.ID, 'createAccountSubmit')
+
+//*Conditions of use link
+driver.find_element(By.XPATH, "//a[text()='Conditions of Use' and @href='/gp/help/customer/display.html/ref=ap_desktop_footer_cou?ie=UTF8&nodeId=508088']")
+
+//*Privacy Notice link
+driver.find_element(By.XPATH, "//a[text()='Privacy Notice' and @href='/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?ie=UTF8&nodeId=468496']")
+
+
+"""
+
+
+
+
+
